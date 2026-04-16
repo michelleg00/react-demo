45e419fc888b81818a16015e3167deb32b69e479
MICHELLE GOSHA <MICHELLEGOSHAX@gmail.com>
2026-04-16 16:48:28 +0200

    tailwind install

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,9 +1,9 @@
 function Hero(props) {
     return (
     <div className="hero-text">
-        <h2>Hello, my name is {props.name}!</h2>
+        <h1>Hello, my name is {props.name}!</h1>
         <h3>Digital Product Designer</h3>
-        <p> I design clean, user-friendly digital experiences. </p>
+        <p>I design clean, user-friendly digital experiences.</p>
         </div>
     );
 }
