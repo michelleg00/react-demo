b927433257b67c5489540cc7821d71398d12f944
MICHELLE GOSHA <MICHELLEGOSHAX@gmail.com>
2026-04-27 17:23:32 +0200

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="flex justify-center items-center gap-4 text-sm text-neutral-400">
+                <span>© 2026 by Michelle Gosha</span>
+                <span>Imprint</span>
+            </div>
+        </footer>
+    );
+}
